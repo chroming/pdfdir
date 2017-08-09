d5b6dac1531b8b68b244f6f15f9ed4203a930d07
chroming <guangzhishang@gmail.com>
2017-08-09 19:19:09 +0800

    Add bookmark to pdf file.

---
+++ pdf/bookmark.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+Add directory bookmarks to the pdf file.
+
+Public:
+
+- function: add_bookmark(path, index_dict)
+
+"""
+
+from .api import Pdf
+
+
+def _add_bookmark(pdf, index_dict, parent=None):
+    for title, dic in index_dict.items():
+        paren = pdf.add_bookmark(title, dic['pagenum'], parent=parent)
+        child = dic.get('child', None)
+        if child:
+            return _add_bookmark(child, paren)
+
+
+def add_bookmark(path, index_dict):
+    """
+    Add directory bookmarks to the pdf file.
+    :param path: pdf file path.
+    :param index_dict: bookmarks dict, like {'title0': {'pagenum': 0, 'child': {'title01': {'pagenum': 1}...}}...}
+    """
+    pdf = Pdf(path)
+    _add_bookmark(pdf, index_dict)
+    pdf.save_pdf()
+
