6fb01f7f7de770668217b8b3631a80ddb6594ec0
chroming <guangzhishang@gmail.com>
2017-08-28 20:47:30 +0800

    update english

--- gui/en.ts
+++ gui/en.ts
@@ -1,77 +1,110 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="en" sourcelanguage="zh_CN">
+<!DOCTYPE TS><TS version="2.0" language="en" sourcelanguage="zh_CN">
 <context>
     <name>PDFdir</name>
     <message>
-        <location filename="main_ui.py" line="89"/>
+        <location filename="main_ui.py" line="155"/>
         <source>PDFdir</source>
         <translation>PDFdir</translation>
     </message>
-    <message utf8="true">
-        <location filename="main_ui.py" line="90"/>
+    <message encoding="UTF-8">
+        <location filename="main_ui.py" line="156"/>
         <source>打开</source>
         <translation>Open</translation>
     </message>
-    <message utf8="true">
-        <location filename="main_ui.py" line="91"/>
+    <message encoding="UTF-8">
+        <location filename="main_ui.py" line="162"/>
         <source>写入导航</source>
         <translation>write</translation>
     </message>
-    <message utf8="true">
-        <location filename="main_ui.py" line="92"/>
+    <message encoding="UTF-8">
+        <location filename="main_ui.py" line="163"/>
         <source>PDF文件路径</source>
-        <translation>PDF path</translation>
+        <translation>PDF file path</translation>
     </message>
-    <message utf8="true">
-        <location filename="main_ui.py" line="93"/>
+    <message encoding="UTF-8">
+        <location filename="main_ui.py" line="164"/>
         <source>目录文本</source>
-        <translation>dir text</translation>
+        <translation>directory text</translation>
     </message>
-    <message utf8="true">
-        <location filename="main_ui.py" line="94"/>
+    <message encoding="UTF-8">
+        <location filename="main_ui.py" line="165"/>
         <source>偏移页</source>
         <translation>offset</translation>
     </message>
     <message>
-        <location filename="main_ui.py" line="95"/>
+        <location filename="main_ui.py" line="166"/>
         <source>0</source>
         <translation>0</translation>
     </message>
-    <message utf8="true">
-        <location filename="main_ui.py" line="96"/>
+    <message encoding="UTF-8">
+        <location filename="main_ui.py" line="176"/>
         <source>帮助</source>
         <translation>Help</translation>
     </message>
-    <message utf8="true">
-        <location filename="main_ui.py" line="97"/>
+    <message encoding="UTF-8">
+        <location filename="main_ui.py" line="177"/>
         <source>语言</source>
         <translation>Language</translation>
     </message>
-    <message utf8="true">
-        <location filename="main_ui.py" line="98"/>
+    <message encoding="UTF-8">
+        <location filename="main_ui.py" line="178"/>
         <source>主页</source>
         <translation>Homepage</translation>
     </message>
-    <message utf8="true">
-        <location filename="main_ui.py" line="99"/>
+    <message encoding="UTF-8">
+        <location filename="main_ui.py" line="179"/>
         <source>帮助手册</source>
         <translation>Usage</translation>
     </message>
-    <message utf8="true">
-        <location filename="main_ui.py" line="100"/>
+    <message encoding="UTF-8">
+        <location filename="main_ui.py" line="180"/>
         <source>检查更新</source>
         <translation>Update</translation>
     </message>
     <message>
-        <location filename="main_ui.py" line="101"/>
+        <location filename="main_ui.py" line="181"/>
         <source>English</source>
         <translation>English</translation>
     </message>
-    <message utf8="true">
-        <location filename="main_ui.py" line="102"/>
+    <message encoding="UTF-8">
+        <location filename="main_ui.py" line="182"/>
         <source>中文</source>
         <translation>中文</translation>
+    </message>
+    <message>
+        <location filename="main_ui.py" line="157"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;SimSun&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="main_ui.py" line="172"/>
+        <source>首层</source>
+        <translation>level 1</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="main_ui.py" line="173"/>
+        <source>二层</source>
+        <translation>level 2</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="main_ui.py" line="174"/>
+        <source>三层</source>
+        <translation>level 3</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="main_ui.py" line="170"/>
+        <source>未识别</source>
+        <translation>unidentified</translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="main_ui.py" line="175"/>
+        <source>子目录</source>
+        <translation>subdirectory</translation>
     </message>
 </context>
 </TS>
