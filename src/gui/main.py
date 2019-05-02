52053df212b9ba0f846a3d4c0e8e258c3401e122
chroming <chroming@live.com>
2019-05-02 19:01:05 +0800

    add crash exception

--- src/gui/main.py
+++ src/gui/main.py
@@ -219,5 +219,12 @@ def run():
     sys.exit(app.exec_())
 
 
+sys._excepthook = sys.excepthook
+def exception_hook(exctype, value, traceback):
+    sys._excepthook(exctype, value, traceback)
+    sys.exit(1)
+sys.excepthook = exception_hook
+
+
 if __name__ == '__main__':
     run()
