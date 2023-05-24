a491f8dbc58aa433e53f2b6d2bc01befe55de5a6
ArtisticZhao <hit.yuhao.zhao@gmail.com>
2023-05-24 21:33:14 +0800

    支持六级目录; 新增命令行运行接口.

--- src/convert.py
+++ src/convert.py
@@ -29,19 +29,20 @@ def is_in(title, exp):
         print("Check regex error! %s" % e)
 
 
-def check_level(title, level0, level1, level2, other=0):
+def check_level(title, level0, level1, level2, level3=None, level4=None, level5=None, other=0):
     """check the level of this title"""
-    if is_in(title, level2):
-        return 2
-    elif is_in(title, level1):
-        return 1
-    elif is_in(title, level0):
-        return 0
+    ls = [level0, level1, level2, level3, level4, level5]
+    for i in range(len(ls)):
+        idx = len(ls)-1 - i  # reserve match
+        if is_in(title, ls[idx]):
+            return idx
+    # no level found
     return other
 
 
-def _convert_dir_text(dir_text, offset=0, level0=None, level1=None, level2=None, other=0):
+def _convert_dir_text(dir_text, offset=0, level0=None, level1=None, level2=None, level3=None, level4=None, level5=None, other=0):
     l0, l1, pagenum, index_dict = 0, 0, -float("inf"), {}
+    l2, l3, l4 = 0, 0, 0
     dir_list = text_to_list(dir_text)
     i = 0
     for di in dir_list:
@@ -49,9 +50,18 @@ def _convert_dir_text(dir_text, offset=0, level0=None, level1=None, level2=None,
         if num > pagenum:
             pagenum = num
         index_dict[i] = {'title': title, 'real_num': pagenum+offset, 'num': pagenum}
-        level = check_level(title, level0, level1, level2, other)
-        if level == 2 and i != l1:
+        level = check_level(title, level0, level1, level2, level3, level4, level5, other=other)
+        if level == 5 and i != l4:
+            index_dict[i]['parent'] = l4
+        elif level == 4 and i != l3:
+            index_dict[i]['parent'] = l3
+            l4 = i
+        elif level == 3 and i != l2:
+            index_dict[i]['parent'] = l2
+            l3 = i
+        elif level == 2 and i != l1:
             index_dict[i]['parent'] = l1
+            l2 = i
         elif level == 1 and i != l0:
             index_dict[i]['parent'] = l0
             l1 = i
@@ -61,7 +71,7 @@ def _convert_dir_text(dir_text, offset=0, level0=None, level1=None, level2=None,
     return index_dict
 
 
-def convert_dir_text(dir_text, offset=0, level0=None, level1=None, level2=None, other=0):
+def convert_dir_text(dir_text, offset=0, level0=None, level1=None, level2=None, level3=None, level4=None, level5=None, other=0):
     """
     convert directory text to dict.
 
@@ -70,8 +80,11 @@ def convert_dir_text(dir_text, offset=0, level0=None, level1=None, level2=None, 
     :param: level0: unicode, the expression to find level0 title.
     :param: level1: unicode, the expression to find level1 title.
     :param: level2: unicode, the expression to find level2 title.
+    :param: level3: unicode, the expression to find level3 title.
+    :param: level4: unicode, the expression to find level4 title.
+    :param: level5: unicode, the expression to find level5 title.
     :param: other: unicode, three level can't match title, then this is the level.
     :return: the dict of directory, like {0:{'title':'A', 'pagenum':1}, 1:{'title':'B', pagenum:2, parent: 0} ......}
 
     """
-    return _convert_dir_text(dir_text, offset, level0, level1, level2, other)
+    return _convert_dir_text(dir_text, offset, level0, level1, level2, level3, level4, level5, other=other)
